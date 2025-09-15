b6bf7949f77c68167404c3e0284eeaf32f6fe43a
Lipe1042 <felipeoliveir27@gmail.com>
2025-09-14 21:43:34 -0300

    projeto pronto

---
+++ index.py
@@ -0,0 +1,50 @@
+# Classificador de nivel de heroi 
+
+# variaveis 
+nomedoheroi = "Felipão"
+
+quantidadedexp = 4000
+
+nivel = "" 
+
+
+# estrutura de decisão 
+if quantidadedexp < 1000: 
+    nivel = "Ferro"
+
+
+elif quantidadedexp >= 1001 and quantidadedexp <= 2000:
+    nivel = "Bronze"
+
+
+elif quantidadedexp >= 2001 and quantidadedexp <= 5000:
+    nivel = "Prata"
+
+
+elif quantidadedexp >= 6001 and quantidadedexp <= 7000:
+    nivel = "Ouro"
+
+
+elif quantidadedexp >= 7001 and quantidadedexp <= 8000:
+    nivel = "Platina"
+
+
+elif quantidadedexp >= 8001 and quantidadedexp <= 9000:
+    nivel = "Ascendente"
+
+
+elif quantidadedexp >= 9001 and quantidadedexp <= 10000:
+    nivel = "Imortal"
+
+
+elif quantidadedexp >= 10001:
+    nivel = "Radiante"
+
+
+
+# resultado 
+print(f"O Herói de nome {nomedoheroi} está no nível de {nivel}")
+
+
+
+
